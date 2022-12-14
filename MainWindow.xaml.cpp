15abd85fe050ba5743b9c9b59020e91bc4dce688
Tranquility0118 <108786095+Tranquility0118@users.noreply.github.com>
2022-12-15 08:14:27 +0900

    초기 커밋.

---
+++ MainWindow.xaml.cpp
@@ -0,0 +1,47 @@
+﻿#include "pch.h"
+#include "MainWindow.xaml.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::help_PTR::implementation
+{
+    MainWindow::MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    int32_t MainWindow::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    /*void MainWindow::myButton_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        myButton().Content(box_value(L"Clicked"));
+    }*/
+}
+
+
+/*void winrt::help_PTR::implementation::MainWindow::rc_RefreshRequested(winrt::Microsoft::UI::Xaml::Controls::RefreshContainer const& sender, winrt::Microsoft::UI::Xaml::Controls::RefreshRequestedEventArgs const& args)
+{
+
+}*/
+
+
+void winrt::help_PTR::implementation::MainWindow::rc_RefreshRequested(winrt::Microsoft::UI::Xaml::Controls::RefreshContainer const& sender, winrt::Microsoft::UI::Xaml::Controls::RefreshRequestedEventArgs const& args)
+{
+    lv().Items().Append(box_value(L"New information on the Top"));
+    lv().Items().InsertAt(0, box_value(L"New information on the Bottom"));
+}
---
+++ MainWindow.xaml.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "MainWindow.g.h"
+
+namespace winrt::help_PTR::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        //void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+        //void rc_RefreshRequested(winrt::Microsoft::UI::Xaml::Controls::RefreshContainer const& sender, winrt::Microsoft::UI::Xaml::Controls::RefreshRequestedEventArgs const& args);
+        void rc_RefreshRequested(winrt::Microsoft::UI::Xaml::Controls::RefreshContainer const& sender, winrt::Microsoft::UI::Xaml::Controls::RefreshRequestedEventArgs const& args);
+    };
+}
+
+namespace winrt::help_PTR::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
